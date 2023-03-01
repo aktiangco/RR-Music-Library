a6907cdb5fa976f4987b217572def83ffdcd42a9
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-02-28 23:50:41 -0800

    hi

--- src/components/ArtistView.jsx
+++ src/components/ArtistView.jsx
@@ -5,13 +5,35 @@ function ArtistView() {
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData)
+        }
+        fetchData()
+    }, [id])
 
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key={i} >
+                <p>{album.collectionName}</p>
+            </div>
+        )
+    })
+    
     return (
         <div>
             <h2>The id passed was: {id}</h2>
             <p>Artist Data Goes Here!</p>
+            {renderAlbums}
         </div>
     )
+    
 }
 
 export default ArtistView
