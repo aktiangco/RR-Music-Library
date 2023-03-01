5d74e006e625df3bb5525c947ef4831ca9c7aa4a
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-03-01 00:24:21 -0800

    developing the Component with useParams

--- src/components/AlbumView.jsx
+++ src/components/AlbumView.jsx
@@ -1,12 +1,15 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about a given album
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
-function AlbumView() {
+const AlbumView = () => {
+    const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
 
     return (
         <div>
+            <p>The id passed was: {id}</p>
             <p>Album Data Goes Here!</p>
         </div>
     )
--- src/components/ArtistView.jsx
+++ src/components/ArtistView.jsx
@@ -1,12 +1,15 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about our artist
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
-function ArtistView() {
+const ArtistView = () => {
+    const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
 
     return (
         <div>
+            <p>The id passed was: {id}</p>
             <p>Artist Data Goes Here!</p>
         </div>
     )
